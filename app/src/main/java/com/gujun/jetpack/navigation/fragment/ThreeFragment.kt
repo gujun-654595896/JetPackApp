74c48d6e99fbe5754dedc61f4a43bfd11f4fc0d9
admin <gujun@cnlive.com>
2021-02-04 14:58:09 +0800

    添加Navigation组件页面返回处理逻辑

--- app/src/main/java/com/gujun/jetpack/navigation/fragment/ThreeFragment.kt
+++ app/src/main/java/com/gujun/jetpack/navigation/fragment/ThreeFragment.kt
@@ -5,31 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import com.gujun.jetpack.R
-import kotlinx.android.synthetic.main.fragment_navigation_two.*
+import kotlinx.android.synthetic.main.fragment_navigation_three.*
 
 /**
  *    author : gujun
  *    date   : 2021/2/2 16:18
  *    desc   :
  */
-class TwoFragment : Fragment() {
+class ThreeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_navigation_two, container, false)
+        return inflater.inflate(R.layout.fragment_navigation_three, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        toFragmentThree.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_twoFragment_to_threeFragment)
+        back.setOnClickListener {
+            NavHostFragment.findNavController(this).popBackStack()
         }
-
     }
 }
